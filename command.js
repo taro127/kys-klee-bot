76d6d2db927c689f3de5c39a8b7da8e57ea04eaf
Glitch (voracious-uttermost-grapple) <none>
2023-07-12 19:12:20 +0000

    🍰💇 Checkpoint ./command.js:43169848/1386 ./main.js:43169848/1688

--- command.js
+++ command.js
@@ -1,19 +1,21 @@
 module.exports = {
-  name: 'command',
+  name: "command",
   description: "Embeds!",
   execute(message, args, Discord) {
     const newEmbed = new Discord.MessageEmbed()
-    .setColor('#FFA0A0')
-    .setTitle('Test')
-    .setDescription('pebis')
-    .addFields(
-      {name: 'deez nuts', value: 'meow'},
-      {name: 'deez nuts', value: 'meow'},
-      {name: 'deez nuts', value: 'meow'}
+      .setColor("#FFA0A0")
+      .setTitle("Test")
+      .setDescription("pebis")
+      .addFields(
+        { name: "deez nuts", value: "meow" },
+        { name: "deez nuts", value: "meow" },
+        { name: "deez nuts", value: "meow" }
       )
-    .setImage('https://images-ext-1.discordapp.net/external/tnwPYA6Az2wcEwfzu7YbuUQYxQbVxKQTIAmqbXDoKAg/%3Fsize%3D512/https/cdn.discordapp.com/splashes/1117771689309257879/903a17d8f6bd2f94a0f00d49cb3fb5bb.jpg?width=819&height=819')
-    .setFooter('test');
+      .setImage(
+        "https://images-ext-1.discordapp.net/external/tnwPYA6Az2wcEwfzu7YbuUQYxQbVxKQTIAmqbXDoKAg/%3Fsize%3D512/https/cdn.discordapp.com/splashes/1117771689309257879/903a17d8f6bd2f94a0f00d49cb3fb5bb.jpg?width=819&height=819"
+      )
+      .setFooter("test");
 
     message.channel.send(newEmbed);
-  }
-}
+  },
+};
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const prefix = '+';
+const prefix = ('+');
 
 const fs = require('fs');
 
@@ -15,21 +15,19 @@ for(const file of commandFiles){
 client.commands.set(command.name, command);
 }
 
-client.on('ready', () => {
-  cosnole.log('Fish Bombing!');
-}};
+client.on('message'), message => {
 
-client.on('message', message => {
-
-          if(!message.content.startsWith(prefix) || message.author.bot) return;
+          if(!message.content.startsWith(prefix) || message.author.bot) return
 
           const args = message.content.slide(prefix.length).split(/ +/);
           const command = args.shift().toLowercase();
-
+  
           if(command === 'command'){
 
-  client.commands.get('command').execute(message, args, Discord);
+  client.commands.get('command').execute(message, args, Discord)
           }
-}};
-
-client.login.('MTEyNzc1NDMyNzE2MzkzNjkwOQ.G67SDP.HkdnM7A1OZNTxfd1sy3RqE__-eRHkXLcTDR_7g');
+  token='MTEyNzc1NDMyNzE2MzkzNjkwOQ.G67SDP.HkdnM7A1OZNTxfd1sy3RqE__-eRHkXLcTDR_7g'
+}
+client.on('ready', => {
+  cosnole.log('Fish Bombing!')
+}

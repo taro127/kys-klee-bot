86be47f7b7aabc64d11336a7ce584abc64e1a480
Glitch (voracious-uttermost-grapple) <none>
2023-07-12 19:24:19 +0000

    👲👰 Checkpoint ./main.js:43169848/1942 ./index.js:43169848/1222 ./package.json:43169848/1239

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-{
-  "name": "klee bot die",
-  "version": "1.0.0",
-  "description": "bombs",
-  "main": "main.js",
-  "scripts": {
-    "start": "node main.js"
-  },
-  "author": "yuyu",
-  "license": "ISC",
-  "dependencies": {
-    "@discordjs/rest": "^0.3.0",
-    "discord-modals": "^1.3.5",
-    "discord.js": "^13.6.0",
-    "fs": "^0.0.1-security",
-    "mysql": "^2.18.1",
-    "node-cron": "^3.0.0"
-  }
-}
+const http = require("http");
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("<h1>Hello World!</h1>"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080);
+//Server runs on localhost:8080
--- main.js
+++ main.js
@@ -1,33 +1,32 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-const prefix = ('+');
+const prefix = "+";
 
-const fs = require('fs');
+const fs = require("fs");
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-  const command = require('./commands/${file}');
+const commandFiles = fs
+  .readdirSync("./commands/")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require("./commands/${file}");
 
-client.commands.set(command.name, command);
+  client.commands.set(command.name, command);
 }
 
-client.on('message'), message => {
+client.on("message"),
+  (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-          if(!message.content.startsWith(prefix) || message.author.bot) return
+    const args = message.content.slide(prefix.length).split(/ +/);
+    const command = args.shift().toLowercase();
 
-          const args = message.content.slide(prefix.length).split(/ +/);
-          const command = args.shift().toLowercase();
-  
-          if(command === 'command'){
-
-  client.commands.get('command').execute(message, args, Discord)
-          }
-  token='MTEyNzc1NDMyNzE2MzkzNjkwOQ.G67SDP.HkdnM7A1OZNTxfd1sy3RqE__-eRHkXLcTDR_7g'
-}
-client.on('ready', => {
-  cosnole.log('Fish Bombing!')
-}
+    if (command === "command") {
+      client.commands.get("command").execute(message, args, Discord);
+    }
+    client.login =
+      "MTEyNzc1NDMyNzE2MzkzNjkwOQ.G67SDP.HkdnM7A1OZNTxfd1sy3RqE__-eRHkXLcTDR_7g";
+  };
